ed7be6abb2f2f773b988cde246368a88eeaca11f
nouuuha <173621209+niylii@users.noreply.github.com>
2025-11-28 18:28:33 +0100

    script done !!

--- configuration/monitoring.sh
+++ configuration/monitoring.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-wall "
-	#Architecture: $(uname -a)
-	#CPU physical: $(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
-	#vCPU: $(grep -c "processor" /proc/cpuinfo)
-	#Memory Usage: $(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)", $3,$2,$3*100/$2}')
-###	#Disk Usage: $(df -h / | awk 'NR==2{printf "%s/%s (%s)", $3,$2,$5}')
-###	#CPU load: $(top -bn1 | grep '^%Cpu' | awk '{printf("%.1f%%"), 100 - $8}')
-	#Last boot: $(who -b | awk '{print $3, $4}')
-	#LVM use: $(if [ $(lsblk | grep -c "lvm") -gt 0 ]; then echo "yes"; else echo "no"; fi)
-	#Connections TCP: $(ss -t | grep -c ESTAB) ESTABLISHED
-	#User log: $(users | tr ' ' '\n' | sort -u | wc -w)
-	#Network: IP $(hostname -I)($(ip link show | grep "link/ether" | awk '{print $2}'))
-	#Sudo: $(journalctl _COMM=sudo | grep -c COMMAND) cmd
-"
-echo $wall | wall
+while true; do 
 
+	#disk usage:
+	used=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{used+=$3} END {print used}')
+	all=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{all+=$2} END {print int(all / 1024)}')
+	per=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{used += $3} {all += $2} END { per= int(used/all * 100); print per}')
+ 	wall "
+		#Architecture: $(uname -a)
+		#CPU physical: $(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
+		#vCPU: $(grep -c "processor" /proc/cpuinfo)
+		#Memory Usage: $(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)", $3,$2,$3*100/$2}')
+		#Disk Usage: $used/$all Gb ($per%)
+		#CPU load: $(mpstat 1 2 | tail -1 | awk '{printf("%.1f%%"), 100 - $12}')
+		#Last boot: $(who -b | awk '{print $3, $4}')
+		#LVM use: $(if [ $(lsblk | grep -c "lvm") -gt 0 ]; then echo "yes"; else echo "no"; fi)
+		#Connections TCP: $(ss -t | grep -c ESTAB) ESTABLISHED
+		#User log: $(users | tr ' ' '\n' | sort -u | wc -w)
+		#Network: IP $(hostname -I)($(ip link show | grep "link/ether" | awk '{print $2}'))
+		#Sudo: $(journalctl -q _COMM=sudo | grep -c COMMAND) cmd
+	"
+	sleep 10m
+done
